4351147ec7af9a397543e6e3467e321c8e3cec25
Andrew Kane <acekane1@gmail.com>
2017-11-08 09:04:40 -0800

    Updated docs [skip ci]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.2 [unreleased]
+
+- Better support Mongoid `BSON::ObjectId`
+
 ## 0.3.1
 
 - Fixed `stack level too deep` error
--- lib/pretender.rb
+++ lib/pretender.rb
@@ -44,6 +44,7 @@ module Pretender
 
       define_method :"impersonate_#{scope}" do |resource|
         instance_variable_set(impersonated_var, resource)
+        # use to_s for Mongoid for BSON::ObjectId
         session[session_key] = resource.id.is_a?(Numeric) ? resource.id : resource.id.to_s
       end
 
