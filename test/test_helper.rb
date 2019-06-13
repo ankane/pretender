a9802c827f5980f953f8b7e3eebecce640fbec7a
Andrew Kane <andrew@chartkick.com>
2019-06-13 00:32:25 -0700

    Improved tests [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 require "bundler/setup"
-require "combustion"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
+require "combustion"
 
 Combustion.path = "test/internal"
-Combustion.initialize! :all do
+Combustion.initialize! :active_record do
   if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
